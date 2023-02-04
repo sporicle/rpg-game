63439635de8be8169c483bee69fc6d2ae4c76169
fauxfire <andyweng97@gmail.com>
2023-02-03 22:01:41 -0800

    added tall grass encounters

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,11 +6,13 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
     public LayerMask solidObjectLayer;
+    public LayerMask grassLayer;
 
     private bool isMoving;
     private Vector2 input;
 
     private Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,17 +27,19 @@ public class PlayerController : MonoBehaviour
             input.x = Input.GetAxisRaw("Horizontal");
             input.y = Input.GetAxisRaw("Vertical");
 
-            if (input.x != 0) input.y = 0;
+            if (input.x != 0)
+                input.y = 0;
 
-            if (input != Vector2.zero) {
+            if (input != Vector2.zero)
+            {
                 animator.SetFloat("moveX", input.x);
                 animator.SetFloat("moveY", input.y);
-                
+
                 var targetPos = transform.position;
                 targetPos.x += input.x;
                 targetPos.y += input.y;
 
-                if (is_walkable(targetPos)) 
+                if (is_walkable(targetPos))
                     StartCoroutine(Move(targetPos));
             }
         }
@@ -43,20 +47,42 @@ public class PlayerController : MonoBehaviour
         animator.SetBool("isMoving", isMoving);
     }
 
-    IEnumerator Move(Vector3 targetPos) {
+    IEnumerator Move(Vector3 targetPos)
+    {
         isMoving = true;
-        while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon) {
-            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+        while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
+        {
+            transform.position = Vector3.MoveTowards(
+                transform.position,
+                targetPos,
+                moveSpeed * Time.deltaTime
+            );
             yield return null;
         }
         transform.position = targetPos;
         isMoving = false;
+
+        CheckForEncounters();
     }
 
-    private bool is_walkable(Vector3 targetPos) {
-        if (Physics2D.OverlapCircle(targetPos, 0.3f, solidObjectLayer) != null){
+    private bool is_walkable(Vector3 targetPos)
+    {
+        if (Physics2D.OverlapCircle(targetPos, 0.3f, solidObjectLayer) != null)
+        {
             return false;
         }
         return true;
+    }
+
+    private void CheckForEncounters()
+    {
+        if (Physics2D.OverlapCircle(transform.position, 0.2f, grassLayer) != null) 
+        {
+            if (Random.Range(1,101) <= 10)
+            {
+                Debug.Log("Encountered a wild pokemon");
+            }
+
+        }
     }
 }
